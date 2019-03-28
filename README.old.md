0dc96f18e2644f04df3dbf0248b969cef3b32905
Hilal <hilalarsa@gmail.com>
2019-03-28 17:35:57 +0700

    Change the web base from vue to react again, vue is hard man

---
+++ README.old.md
@@ -0,0 +1,11 @@
+This is Readme file
+
+The page is supposedly to pe a single page app developed using Vue.js.
+
+But I changed it!
+
+
+TO GATSBY!
+
+
+...because vue is hard
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {Drawer, AppBar, Toolbar} from '@material-ui/core';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Permanent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
