5b39a675425ce6237ba4d55ac1afa029d30a7d2a
Hilal <hilalarsa@gmail.com>
2019-03-30 22:38:24 +0700

    Add react package

--- src/App.js
+++ src/App.js
@@ -1,53 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import {Drawer, AppBar, Toolbar} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <div className="App">
-        <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Permanent drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+        
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
 
 
